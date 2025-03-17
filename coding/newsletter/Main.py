dcd6fbe0d948cc124d2c6b630a17ca4eb29dd0e1
jinsoo <jensoo7023@gmail.com>
2025-03-17 18:10:23 +0900

    add housing news

--- coding/newsletter/Main.py
+++ coding/newsletter/Main.py
@@ -2,9 +2,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from config import Config
-from crawling import NewsCrawler
-from htmlGenerator import HTMLGenerator
-
+from newsCrawling import NewsCrawler
+from blogCrawling import BlogCrawler
+from newsHtmlGenerator import NewsHTMLGenerator
+from blogHtmlGenerator import BlogHtmlGenerator
 
 def send_email(subject, html_content, recipients):
     """Send email using SMTP."""
@@ -39,17 +40,23 @@ def send_email(subject, html_content, recipients):
 
 if __name__ == "__main__":
     # Recipients
-    recipients = ["jensoo7023@naver.com", "qejlfn@naver.com"]
+    news_recipients = ["jensoo7023@naver.com", "qejlfn@naver.com"]
+    blog_recipients = ["qejlfn@naver.com","jensoo7023@gmail.com"]
 
     # Step 1: Fetch news
-    crawler = NewsCrawler()
-    news_data = crawler.fetch_news()
+    news_crawler = NewsCrawler()
+    news_data = news_crawler.fetch_news()
+    blog_crawler = BlogCrawler()
+    blog_data = blog_crawler.fetch_news()
 
     # Step 2: Generate HTML
-    html_generator = HTMLGenerator()
-    final_html = html_generator.generate_html(news_data)
+    news_html_generator = NewsHTMLGenerator()
+    final_html = news_html_generator.generate_html(news_data)
+    blog_html_generator = BlogHtmlGenerator()
+    blog_html = blog_html_generator.generate_html(blog_data)
 
     # Step 3: Send email
-    send_email("오늘의 주요 경제뉴스", final_html, recipients)
+    #send_email("오늘의 주요 경제뉴스", final_html, news_recipients)
+    send_email("이번주 청약 소식", blog_html, blog_recipients)
 
 
